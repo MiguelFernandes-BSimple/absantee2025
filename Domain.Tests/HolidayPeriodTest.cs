2c851dad9789ef308387decd85944b72720fdbb7
Tiago Gouveia <tiagogouveia@b-simple.local>
2025-03-21 11:26:24 +0000

    fix: added test for same day holiday

--- Domain.Tests/HolidayPeriodTest.cs
+++ Domain.Tests/HolidayPeriodTest.cs
@@ -7,6 +7,7 @@ public class HolidayPeriodTest {
     {
         yield return new object[] { DateOnly.FromDateTime(DateTime.Now), DateOnly.FromDateTime(DateTime.Now.AddDays(1)) };
         yield return new object[] { DateOnly.FromDateTime(DateTime.Now.AddYears(1)), DateOnly.FromDateTime(DateTime.Now.AddYears(2)) };
+        yield return new object[] { DateOnly.FromDateTime(DateTime.Now), DateOnly.FromDateTime(DateTime.Now) };
     }
 
     [Theory]
