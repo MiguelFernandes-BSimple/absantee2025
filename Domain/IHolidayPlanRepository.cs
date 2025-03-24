b3880d62f4907fe35890e7c5ea8908d8e1f09a47
Fernando Silva <fernando.silva@b-simple.com>
2025-03-24 16:01:32 +0000

    IHolidayPlanRepository created

---
+++ Domain/IHolidayPlanRepository.cs
@@ -0,0 +1,18 @@
+using Domain;
+
+public interface IHolidayPlanRepository
+{
+    public IEnumerable<IHolidayPeriod> FindHolidayPeriodsForCollaboratorBetween(IColaborator colaborator, DateOnly initDate, DateOnly endDate);
+    public IEnumerable<IColaborator> FindAllCollaboratorsWithHolidayPeriodsBetween(DateOnly initDate, DateOnly endDate);
+    public IEnumerable<IColaborator> FindAllCollaboratorsWithHolidayPeriodsLongerThan(int days);
+    public int GetHolidayDaysInProject(IProject project);
+    public IHolidayPeriod GetHolidayPeriodContainingDate(IColaborator colaborator, DateOnly date);
+    public IEnumerable<IHolidayPeriod> FindHolidayPeriodsForCollaboratorBetweenLongerThan(IColaborator colaborator, DateOnly initDate, DateOnly endDate, int days);
+    public IEnumerable<IHolidayPeriod> FindHolidayPeriodsForCollaboratorThatIncludeWeekends(IColaborator colaborator);
+    public IEnumerable<IHolidayPeriod> FindOverlappingHolidayPeriodsForCollaboratorsBetween(IColaborator colaborator1, IColaborator colaborator2, DateOnly initDate, DateOnly endDate);
+    public IEnumerable<IHolidayPeriod> FindHolidayPeriodsForProjectCollaboratorsBetween(IProject project, DateOnly initDate, DateOnly endDate);
+    //DUVIDA ASSOCIAR?
+    public int GetHolidayDaysForProjectCollaboratorBetween(AssociationProjectColaborator association, DateOnly initDate, DateOnly endDate);
+    public int GetHolidayDaysForAllProjectCollaboratorsBetween(IProject project, DateOnly initDate, DateOnly endDate);
+
+}
