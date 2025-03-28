3b6b0e1ba2dc2b1033dd17edbff2b94f0d3add63
miguelalecrim-coder <miguel.alecrim@b-simple.pt>
2025-03-28 17:48:44 +0000

    US23 new implementation

--- Domain/HolidayPlanService.cs
+++ Domain/HolidayPlanService.cs
@@ -25,5 +25,43 @@ namespace Domain
             this.associationProjectCollaboratorRepository = associationProjectCollaboratorRepository;
             this.holidayPlanRepository = holidayPlanRepository;
         }
+
+
+        public int GetHolidayDaysForProjectCollaboratorBetweenDates(IProject project, DateOnly initDate, DateOnly endDate)
+        {
+            if (initDate > endDate || holidayPlanRepository == null || associationProjectCollaboratorRepository == null)
+            {
+                return 0;
+            }
+
+            var collaborators = associationProjectCollaboratorRepository.FindAllProjectCollaborators(project);
+            int totalHolidayDays = 0;
+
+            foreach (var collaborator in collaborators)
+            {
+                var holidayPlans = holidayPlanRepository.GetHolidayPlansByCollaborator(collaborator);
+
+                foreach (var holidayPlan in holidayPlans)
+                {
+                    foreach (var holidayPeriod in holidayPlan.GetHolidayPeriods())
+                    {
+                        var holidayStart = holidayPeriod.GetInitDate();
+                        var holidayEnd = holidayPeriod.GetFinalDate();
+
+                        if (holidayEnd < initDate || holidayStart > endDate)
+                        {
+                            continue;
+                        }
+
+                        var effectiveStart = holidayStart < initDate ? initDate : holidayStart;
+                        var effectiveEnd = holidayEnd > endDate ? endDate : holidayEnd;
+
+                        totalHolidayDays += effectiveEnd.DayNumber - effectiveStart.DayNumber + 1;
+                    }
+                }
+            }
+            
+            return totalHolidayDays;
+        }
     }
-}
+}   
