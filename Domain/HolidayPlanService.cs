8bfcd94280e60728d8f2fe1de33d2d0d22cf2bc9
Tiago Branco <tiago.branco@b-simple.pt>
2025-03-31 18:04:25 +0100

    moved repository uc19 and uc20 methods to service + tests added (missing some cases)

--- Domain/HolidayPlanService.cs
+++ Domain/HolidayPlanService.cs
@@ -7,23 +7,67 @@ namespace Domain
 {
     public class HolidayPlanService
     {
-        private IAssociationProjectCollaboratorRepository? associationProjectCollaboratorRepository;
-        private IHolidayPlanRepository? holidayPlanRepository;
-
-        public HolidayPlanService(IAssociationProjectCollaboratorRepository associationProjectCollaboratorRepository)
-        {
-            this.associationProjectCollaboratorRepository = associationProjectCollaboratorRepository;
-        }
+        private IAssociationProjectCollaboratorRepository? _associationProjectCollaboratorRepository;
+        private IHolidayPlanRepository _holidayPlanRepository;
 
         public HolidayPlanService(IHolidayPlanRepository holidayPlanRepository)
         {
-            this.holidayPlanRepository = holidayPlanRepository;
+            _holidayPlanRepository = holidayPlanRepository;
         }
 
         public HolidayPlanService(IAssociationProjectCollaboratorRepository associationProjectCollaboratorRepository, IHolidayPlanRepository holidayPlanRepository)
         {
-            this.associationProjectCollaboratorRepository = associationProjectCollaboratorRepository;
-            this.holidayPlanRepository = holidayPlanRepository;
+            this._associationProjectCollaboratorRepository = associationProjectCollaboratorRepository;
+            this._holidayPlanRepository = holidayPlanRepository;
+        }
+
+        // UC19 - Given a collaborator and a period to search for, return the holiday periods that contain weekends.
+        public IEnumerable<IHolidayPeriod> FindAllHolidayPeriodsForCollaboratorBetweenDatesThatIncludeWeekends(
+            ICollaborator collaborator,
+            DateOnly searchInitDate,
+            DateOnly searchEndDate
+        )
+        {
+            if (!Utils.ContainsWeekend(searchInitDate, searchEndDate))
+                return Enumerable.Empty<IHolidayPeriod>();
+
+            IEnumerable<IHolidayPeriod> holidayPeriodsBetweenDates =
+                _holidayPlanRepository.FindAllHolidayPeriodsForCollaboratorBetweenDates(collaborator, searchInitDate, searchEndDate);
+
+            IEnumerable<IHolidayPeriod> hp = holidayPeriodsBetweenDates.Where(holidayPeriod =>
+            {
+                DateOnly intersectionStart = Utils.DataMax(holidayPeriod.GetInitDate(), searchInitDate);
+                DateOnly intersectionEnd = Utils.DataMin(holidayPeriod.GetFinalDate(), searchEndDate);
+                return intersectionStart <= intersectionEnd
+                    && Utils.ContainsWeekend(intersectionStart, intersectionEnd);
+            });
+
+            return hp;
+        }
+
+        // UC20 - Given 2 collaborators and a period to search for, return the overlapping holiday periods they have.
+        public IEnumerable<IHolidayPeriod> FindAllOverlappingHolidayPeriodsBetweenTwoCollaboratorsBetweenDates(
+            ICollaborator collaborator1,
+            ICollaborator collaborator2,
+            DateOnly searchInitDate,
+            DateOnly searchEndDate
+        )
+        {
+            IEnumerable<IHolidayPeriod> holidayPeriodListColab1 =
+                _holidayPlanRepository.FindAllHolidayPeriodsForCollaboratorBetweenDates(collaborator1, searchInitDate, searchEndDate);
+
+            IEnumerable<IHolidayPeriod> holidayPeriodListColab2 =
+                _holidayPlanRepository.FindAllHolidayPeriodsForCollaboratorBetweenDates(collaborator2, searchInitDate, searchEndDate);
+
+            IEnumerable<IHolidayPeriod> hp = holidayPeriodListColab1
+                .SelectMany(period1 => holidayPeriodListColab2
+                        .Where(period2 =>
+                            period1.GetInitDate() <= period2.GetFinalDate()
+                            && period1.GetFinalDate() >= period2.GetInitDate())
+                        .SelectMany(period2 => new List<IHolidayPeriod> { period1, period2 }))
+                        .Distinct();
+
+            return hp;
         }
     }
 }
--- Domain/IHolidayPlanRepository.cs
+++ Domain/IHolidayPlanRepository.cs
@@ -7,8 +7,6 @@ public interface IHolidayPlanRepository
     public int GetHolidayDaysOfCollaboratorInProject(IAssociationProjectCollaborator association);
     public IHolidayPeriod? GetHolidayPeriodContainingDate(ICollaborator collaborator, DateOnly date);
     public IEnumerable<IHolidayPeriod> FindAllHolidayPeriodsLongerThanForCollaboratorBetweenDates(ICollaborator collaborator, DateOnly initDate, DateOnly endDate, int days);
-    public IEnumerable<IHolidayPeriod> FindAllHolidayPeriodsForCollaboratorBetweenDatesThatIncludeWeekends(ICollaborator collaborator, DateOnly initDate, DateOnly endDate);
-    public IEnumerable<IHolidayPeriod> FindAllOverlappingHolidayPeriodsBetweenTwoCollaboratorsBetweenDates(ICollaborator collaborator1, ICollaborator collaborator2, DateOnly initDate, DateOnly endDate);
     //public IEnumerable<IHolidayPeriod> FindAllHolidayPeriodsForAllProjectCollaboratorsBetweenDates(IProject project, DateOnly initDate, DateOnly endDate);
     public int GetHolidayDaysForProjectCollaboratorBetweenDates(IAssociationProjectCollaborator association, DateOnly initDate, DateOnly endDate);
     public int GetHolidayDaysForAllProjectCollaboratorsBetweenDates(IEnumerable<ICollaborator> collaborators, DateOnly initDate, DateOnly endDate);
