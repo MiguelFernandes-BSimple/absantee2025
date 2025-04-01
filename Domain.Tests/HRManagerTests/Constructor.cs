b3137dab52c62427604a4f3874c51f7589c71a70
Bruno <bruno.lopes@b-simple.pt>
2025-04-01 15:11:17 +0100

    hrmanager and project manager tests

---
+++ Domain.Tests/HRManagerTests/Constructor.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Domain.Tests.HRManagerTests
+{
+    public class Constructor
+    {
+        public static IEnumerable<object[]> ValidData()
+        {
+            yield return new object[] { DateTime.Now, DateTime.Now.AddDays(10) };
+            yield return new object[] { DateTime.Now.AddDays(10), DateTime.Now.AddDays(20) };
+            yield return new object[] { DateTime.Now.AddYears(1), DateTime.Now.AddYears(2) };
+            yield return new object[] { DateTime.Now, null! };
+            yield return new object[] { DateTime.Now, DateTime.Now };
+            yield return new object[] { DateTime.Now, DateTime.MaxValue };
+            yield return new object[] { DateTime.MinValue, DateTime.Now };
+            yield return new object[] { DateTime.MinValue, DateTime.MaxValue };
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidData))]
+        public void WhenCreatingHRManagerWithValidData_ThenShouldBeInstantiated(DateTime dataInicio, DateTime? dataFim)
+        {
+            //arrange
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(It.IsAny<DateTime>())).Returns(false);
+            user.Setup(u => u.IsDeactivated()).Returns(false);
+
+            //act
+            new HRManager(user.Object, dataInicio, dataFim);
+
+            //assert
+        }
+
+        public static IEnumerable<object[]> InvalidData()
+        {
+            yield return new object[] { DateTime.Now, DateTime.Now.AddDays(-10) };
+            yield return new object[] { DateTime.Now.AddDays(-10), DateTime.Now.AddDays(-20) };
+            yield return new object[] { DateTime.Now.AddYears(-1), DateTime.Now.AddYears(-2) };
+            yield return new object[] { DateTime.MaxValue, DateTime.MinValue };
+            yield return new object[] { DateTime.MinValue, DateTime.MinValue };
+            yield return new object[] { DateTime.MaxValue, DateTime.MaxValue };
+            yield return new object[] { DateTime.MaxValue, null! };
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidData))]
+        public void WhenCreatingHRManagerWithInvalidData_ThenThrowsException(DateTime dataInicio, DateTime dataFim)
+        {
+            //arrange
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(It.IsAny<DateTime>())).Returns(false);
+            user.Setup(u => u.IsDeactivated()).Returns(false);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new HRManager(user.Object, dataInicio, dataFim));
+
+            Assert.Equal("Invalid Arguments", exception.Message);
+        }
+
+        [Fact]
+        public void WhenCreatingHRManagerWithEndDateAfterDeactivationData_ThenThrowsException()
+        {
+            //arrange
+            DateTime dataInicio = DateTime.Now;
+            DateTime dataFim = DateTime.Now.AddDays(10);
+
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(dataFim)).Returns(true);
+            user.Setup(u => u.IsDeactivated()).Returns(false);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new HRManager(user.Object, dataInicio, dataFim));
+
+            Assert.Equal("Invalid Arguments", exception.Message);
+        }
+
+        [Fact]
+        public void WhenCreatingHRManagerWithInactiveUser_ThenThrowsException()
+        {
+            //arrange
+            DateTime dataInicio = DateTime.Now;
+            DateTime dataFim = DateTime.Now.AddDays(10);
+
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(dataFim)).Returns(false);
+            user.Setup(u => u.IsDeactivated()).Returns(true);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new HRManager(user.Object, dataInicio, dataFim));
+
+            Assert.Equal("Invalid Arguments", exception.Message);
+        }
+
+        [Fact]
+        public void WhenCreatingHRManagerWithEndDateAfterDeactivationDataAndInactiveUser_ThenThrowsException()
+        {
+            //arrange
+            DateTime dataInicio = DateTime.Now;
+            DateTime dataFim = DateTime.Now.AddDays(10);
+
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(dataFim)).Returns(true);
+            user.Setup(u => u.IsDeactivated()).Returns(true);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new HRManager(user.Object, dataInicio, dataFim));
+
+            Assert.Equal("Invalid Arguments", exception.Message);
+        }
+    }
+}
---
+++ Domain.Tests/ProjectManagerTests/Constructor.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Domain.Tests.ProjectManagerTests
+{
+    public class Constructor
+    {
+        public static IEnumerable<object[]> GetProjectManager_WithValidDates()
+        {
+            yield return new object[] { DateTime.Now, DateTime.Now.AddDays(1) };
+            yield return new object[] { DateTime.Now, DateTime.Now.AddYears(3) };
+            yield return new object[] { DateTime.Today, DateTime.Today };
+            yield return new object[] { DateTime.Now, null! };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetProjectManager_WithValidDates))]
+        public void WhenGivenValidFields_ThenProjectManagerIsInstantiated(DateTime initDate, DateTime? endDate)
+        {
+            //arrange
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(It.IsAny<DateTime>())).Returns(false);
+            user.Setup(u => u.IsDeactivated()).Returns(false);
+
+            //act
+            new ProjectManager(user.Object, initDate, endDate);
+
+            //assert
+        }
+
+
+        public static IEnumerable<object[]> GetProjectManager_WithInvalidDates()
+        {
+            yield return new object[] { DateTime.Now.AddDays(5), DateTime.Now.AddDays(1) };
+            yield return new object[] { DateTime.Now.AddYears(-1), DateTime.Now.AddYears(-3) };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetProjectManager_WithInvalidDates))]
+        public void WhenGivenInvalidDates_ThenExceptionIsThrown(DateTime initDate, DateTime endDate)
+        {
+            //arrange
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(endDate)).Returns(false);
+            user.Setup(u => u.IsDeactivated()).Returns(false);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new ProjectManager(user.Object, initDate, endDate));
+
+            Assert.Equal("Invalid arguments.", exception.Message);
+        }
+
+        [Fact]
+        public void WhenGivenProjectManagerFinalDateAfterUserDeactivationDate_ThenExceptionIsThrown()
+        {
+            //arrange
+            DateTime initDate = DateTime.Now;
+            DateTime endDate = DateTime.Now.AddDays(5);
+
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(endDate)).Returns(true);
+            user.Setup(u => u.IsDeactivated()).Returns(false);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new ProjectManager(user.Object, initDate, endDate));
+
+            Assert.Equal("Invalid arguments.", exception.Message);
+        }
+
+        [Fact]
+        public void WhenGivenInactiveUser_ThenExceptionIsThrown()
+        {
+            //arrange
+            DateTime initDate = DateTime.Now;
+            DateTime endDate = DateTime.Now.AddDays(5);
+
+            Mock<IUser> user = new Mock<IUser>();
+            user.Setup(u => u.DeactivationDateIsBefore(endDate)).Returns(false);
+            user.Setup(u => u.IsDeactivated()).Returns(true);
+
+            //assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new ProjectManager(user.Object, initDate, endDate));
+
+            Assert.Equal("Invalid arguments.", exception.Message);
+        }
+    }
+}
