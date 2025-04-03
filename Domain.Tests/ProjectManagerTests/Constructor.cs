ef747eeaa3ca47be0379ff55270aed3ed74d715f
Tiago Branco <tiago.branco@b-simple.pt>
2025-04-03 09:38:07 +0100

    ProjectManager updated + tests fix

--- Domain.Tests/ProjectManagerTests/Constructor.cs
+++ Domain.Tests/ProjectManagerTests/Constructor.cs
@@ -2,6 +2,7 @@ using Domain.Interfaces;
 using Domain.Models;
 using Moq;
 
+namespace Domain.Tests.ProjectManagerTests;
 
 public class Constructor
 {
@@ -32,34 +33,6 @@ public class Constructor
         //assert
     }
 
-
-    public static IEnumerable<object[]> GetProjectManager_WithInvalidDates()
-    {
-        yield return new object[] { DateTime.Now.AddDays(5), DateTime.Now.AddDays(1) };
-        yield return new object[] { DateTime.Now.AddYears(-1), DateTime.Now.AddYears(-3) };
-    }
-
-    [Theory]
-    [MemberData(nameof(GetProjectManager_WithInvalidDates))]
-    public void WhenGivenInvalidDates_ThenExceptionIsThrown(DateTime initDate, DateTime endDate)
-    {
-        //arrange
-        Mock<IUser> user = new Mock<IUser>();
-        user.Setup(u => u.DeactivationDateIsBefore(endDate)).Returns(false);
-        user.Setup(u => u.IsDeactivated()).Returns(false);
-
-        Mock<IPeriodDateTime> periodDateTime = new Mock<IPeriodDateTime>();
-        periodDateTime.Setup(p => p.GetInitDate()).Returns(initDate);
-        periodDateTime.Setup(p => p.GetFinalDate()).Returns(endDate);
-
-        //assert
-        ArgumentException exception = Assert.Throws<ArgumentException>(() =>
-            //act
-            new ProjectManager(user.Object, periodDateTime.Object));
-
-        Assert.Equal("Invalid arguments.", exception.Message);
-    }
-
     [Fact]
     public void WhenGivenProjectManagerFinalDateAfterUserDeactivationDate_ThenExceptionIsThrown()
     {
@@ -80,7 +53,7 @@ public class Constructor
             //act
             new ProjectManager(user.Object, periodDateTime.Object));
 
-        Assert.Equal("Invalid arguments.", exception.Message);
+        Assert.Equal("Invalid Arguments.", exception.Message);
     }
 
     [Fact]
@@ -103,6 +76,6 @@ public class Constructor
             //act
             new ProjectManager(user.Object, periodDateTime.Object));
 
-        Assert.Equal("Invalid arguments.", exception.Message);
+        Assert.Equal("Invalid Arguments.", exception.Message);
     }
 }
