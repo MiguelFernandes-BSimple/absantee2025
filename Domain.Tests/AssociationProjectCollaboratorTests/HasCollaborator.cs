981049ea829d14426f01c90b8d7bd68917731756
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-04-03 19:43:58 +0100

    HasCollaborator test fix

--- Domain.Tests/AssociationProjectCollaboratorTests/HasCollaborator.cs
+++ Domain.Tests/AssociationProjectCollaboratorTests/HasCollaborator.cs
@@ -10,21 +10,22 @@ public class HasCollaborator
     public void WhenReceivesSameCollaborator_ReturnTrue()
     {
         //arrange
-        Mock<IProject> ProjectMock = new Mock<IProject>();
-        Mock<IPeriodDate> periodDateMock = new Mock<IPeriodDate>();
+        var projectDouble = new Mock<IProject>();
+        var periodDateDouble = new Mock<IPeriodDate>();
 
-        ProjectMock.Setup(p => p.ContainsDates(periodDateMock.Object)).Returns(true);
+        projectDouble.Setup(p => p.ContainsDates(periodDateDouble.Object)).Returns(true);
+        projectDouble.Setup(c => c.IsFinished()).Returns(false);
 
-        ProjectMock.Setup(c => c.IsFinished()).Returns(false);
+        var collaboratorDouble = new Mock<ICollaborator>();
+        collaboratorDouble.Setup(c => c.ContractContainsDates(It.IsAny<IPeriodDateTime>())).Returns(true);
+        // perguntar se isto tem de ser assim feito ou se temos de mudar o equals na classe AssociationProjectColaborator
+        collaboratorDouble.Setup(c => c.Equals(collaboratorDouble.Object)).Returns(true);
 
-        Mock<ICollaborator> CollaboratorMock = new Mock<ICollaborator>();
-
-        CollaboratorMock.Setup(c => c.ContractContainsDates(It.IsAny<IPeriodDateTime>())).Returns(true);
-
-        var assocProjCollab = new AssociationProjectCollaborator(periodDateMock.Object, CollaboratorMock.Object, ProjectMock.Object);
+        var assocProjCollab = new AssociationProjectCollaborator(periodDateDouble.Object, collaboratorDouble.Object, projectDouble.Object);
 
         //act
-        bool result = assocProjCollab.HasCollaborator(CollaboratorMock.Object);
+        bool result = assocProjCollab.HasCollaborator(collaboratorDouble.Object);
+
         //assert
         Assert.True(result);
     }
