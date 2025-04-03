caceb85e21d7de9fe235c729c966d9035dc8a120
Tiago Branco <tiago.branco@b-simple.pt>
2025-04-03 17:54:32 +0100

    Added tests for Intersect HolidayPeriod override method

--- Domain.Tests/HolidayPeriodTests/Intersects.cs
+++ Domain.Tests/HolidayPeriodTests/Intersects.cs
@@ -1,39 +1,78 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain.Interfaces;
 using Domain.Models;
 using Moq;
 
-namespace Domain.Tests.HolidayPeriodTests
+namespace Domain.Tests.HolidayPeriodTests;
+
+public class Intersects
 {
-    public class Intersects
+    [Fact]
+    public void WhenPassingValidPeriodDate_ThenReturnsTrue()
     {
-        [Fact]
-        public void WhenPassingValidPeriod_ThenReturnsTrue()
-        {
-            var periodDouble = new Mock<IPeriodDate>();
-            periodDouble.Setup(pd => pd.Intersects(It.IsAny<IPeriodDate>())).Returns(true);
+        //arrange
+        var periodDouble = new Mock<IPeriodDate>();
+        periodDouble.Setup(pd => pd.Intersects(It.IsAny<IPeriodDate>())).Returns(true);
 
-            var holidayPeriod = new HolidayPeriod(periodDouble.Object);
+        var holidayPeriod = new HolidayPeriod(periodDouble.Object);
 
-            var result = holidayPeriod.Intersects(periodDouble.Object);
+        //act
+        var result = holidayPeriod.Intersects(periodDouble.Object);
 
-            Assert.True(result);
-        }
+        //assert
+        Assert.True(result);
+    }
 
-        [Fact]
-        public void WhenPassingValidIncorrectPeriod_ThenReturnsFalse()
-        {
-            var periodDouble = new Mock<IPeriodDate>();
-            periodDouble.Setup(pd => pd.Intersects(It.IsAny<IPeriodDate>())).Returns(false);
+    [Fact]
+    public void WhenPassingValidIncorrectPeriodDate_ThenReturnsFalse()
+    {
+        //arrange
+        var periodDouble = new Mock<IPeriodDate>();
+        periodDouble.Setup(pd => pd.Intersects(It.IsAny<IPeriodDate>())).Returns(false);
 
-            var holidayPeriod = new HolidayPeriod(periodDouble.Object);
+        var holidayPeriod = new HolidayPeriod(periodDouble.Object);
 
-            var result = holidayPeriod.Intersects(periodDouble.Object);
+        //act
+        var result = holidayPeriod.Intersects(periodDouble.Object);
 
-            Assert.False(result);
-        }
+        //assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void WhenPassingValidHolidayPeriod_ThenReturnsTrue()
+    {
+        //arrange
+        var periodDate = new Mock<IPeriodDate>();
+        var holidayPeriod = new HolidayPeriod(periodDate.Object);
+
+        var periodDate2 = new Mock<IPeriodDate>();
+        var holidayPeriod2 = new HolidayPeriod(periodDate2.Object);
+
+        periodDate.Setup(pd => pd.Intersects(periodDate2.Object)).Returns(true);
+
+        //act
+        var result = holidayPeriod.Intersects(holidayPeriod2);
+
+        //assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void WhenPassingValidIncorrectHolidayPeriod_ThenReturnsFalse()
+    {
+        //arrange
+        var periodDate = new Mock<IPeriodDate>();
+        var holidayPeriod = new HolidayPeriod(periodDate.Object);
+
+        var periodDate2 = new Mock<IPeriodDate>();
+        var holidayPeriod2 = new HolidayPeriod(periodDate2.Object);
+
+        periodDate.Setup(pd => pd.Intersects(periodDate2.Object)).Returns(false);
+
+        //act
+        var result = holidayPeriod.Intersects(holidayPeriod2);
+
+        //assert
+        Assert.False(result);
     }
 }
