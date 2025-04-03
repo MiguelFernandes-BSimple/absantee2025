dd3f86432261033130210535aa01edff80918593
MiguelFernandes-BSimple <miguel.fernandes@b-simple.pt>
2025-04-03 09:15:38 +0100

    TrainingPeriodTests -> corrgidos

--- Domain.Tests/TrainingPeriodTests/Constructor.cs
+++ Domain.Tests/TrainingPeriodTests/Constructor.cs
@@ -40,66 +40,27 @@ public class Constructor
         //assert
     }
 
-    public static IEnumerable<object[]> GetTrainingPeriodData_InvalidDates()
-    {
-        yield return new object[]
-        {
-            DateOnly.FromDateTime(DateTime.Now).AddDays(5),
-            DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
-        };
-        yield return new object[]
-        {
-            DateOnly.FromDateTime(DateTime.Now).AddYears(4),
-            DateOnly.FromDateTime(DateTime.Now.AddYears(2)),
-        };
-    }
-
-    [Theory]
-    [MemberData(nameof(GetTrainingPeriodData_InvalidDates))]
-    public void WhenPassingStartDateAfterFinalDate_ThenThrowsArgumentException(
-        DateOnly initDate,
-        DateOnly finalDate
-    )
-    {
-        //arrange
-        Mock<IPeriodDate> periodDateMock = new Mock<IPeriodDate>();
-        periodDateMock.Setup(p => p.GetInitDate()).Returns(initDate);
-        periodDateMock.Setup(p => p.GetFinalDate()).Returns(finalDate);
-        //assert
-        ArgumentException exception = Assert.Throws<ArgumentException>(
-            () =>
-                //act
-                new TrainingPeriod(periodDateMock.Object)
-        );
-
-        Assert.Equal("Invalid Arguments", exception.Message);
-    }
-
     public static IEnumerable<object[]> GetTrainingPeriodData_InvalidInitialDate()
     {
         yield return new object[]
         {
             DateOnly.FromDateTime(DateTime.Now).AddDays(-5),
-            DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
         };
         yield return new object[]
         {
             DateOnly.FromDateTime(DateTime.Now).AddYears(-3),
-            DateOnly.FromDateTime(DateTime.Now.AddYears(-1)),
         };
     }
 
     [Theory]
     [MemberData(nameof(GetTrainingPeriodData_InvalidInitialDate))]
     public void WhenPassingDatesInThePast_ThenThrowsArgumentException(
-        DateOnly initDate,
-        DateOnly finalDate
+        DateOnly initDate
     )
     {
         //arrange
         Mock<IPeriodDate> periodDateMock = new Mock<IPeriodDate>();
         periodDateMock.Setup(p => p.GetInitDate()).Returns(initDate);
-        periodDateMock.Setup(p => p.GetFinalDate()).Returns(finalDate);
         //assert
         ArgumentException exception = Assert.Throws<ArgumentException>(
             () =>
