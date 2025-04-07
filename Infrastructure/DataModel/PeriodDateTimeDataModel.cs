7c1a153d3e74b83bc7929e41fc0fa99bd0824967
miguelalecrim-coder <miguel.alecrim@b-simple.pt>
2025-04-07 15:22:09 +0100

    PeriodDataModel + PeriodData

---
+++ Infrastructure/DataModel/PeriodDateTimeDataModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataModel
+{
+    public class PeriodDateTimeDataModel
+    {
+        public long Id { get; set; }
+        public DateOnly _initDate { get; set; }
+        public DateOnly _finalDate { get; set; }
+
+
+        public PeriodDateTimeDataModel( )
+        {}
+
+    }
+}
