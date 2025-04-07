1e6f945d35ee62d5896d5664872895159585ef74
MiguelFernandes-BSimple <miguel.fernandes@b-simple.pt>
2025-04-07 14:51:14 +0100

    Context e WebApi

---
+++ Infrastructure/AbsanteeContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AbsanteeContext : DbContext
+    {
+        public virtual DbSet<Collaborator> Collaborators { get; set; }
+        public virtual DbSet<AssociationProjectCollaborator> Associations  { get; set; }
+        public virtual DbSet<HolidayPlanRepository> HolidayPlans { get; set; }
+        public virtual DbSet<Project> Projects { get; set; }
+
+        public AbsanteeContext(DbContextOptions<AbsanteeContext> options) : base(options) 
+        {
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AbsanteeContext>(opt =>
+    opt.UseNpgsql("postgresql://localhost/absantee")
+    );
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
