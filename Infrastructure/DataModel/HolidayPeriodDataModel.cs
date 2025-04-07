71ba9c911a690ab794889503c2d729d48a76726d
MiguelFernandes-BSimple <miguel.fernandes@b-simple.pt>
2025-04-07 17:25:11 +0100

    ProjectRepository

--- Infrastructure/DataModel/HolidayPeriodDataModel.cs
+++ Infrastructure/DataModel/HolidayPeriodDataModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Infrastructure.DataModel;
 
 public class HolidayPeriodDataModel
---
+++ Infrastructure/Repositories/ProjectRepositoryEF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProjectRepositoryEF : GenericRepository<Project>, IProjectRepository
+    {
+        public ProjectRepositoryEF(AbsanteeContext context) : base(context)
+        {
+        }
+    }
+}
