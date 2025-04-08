c7fc5e3d1da6857e1107f3fb3548327db3fd2d2d
Tiago Branco <tiago.branco@b-simple.pt>
2025-04-08 14:30:46 +0100

    HolidayPeriodDataModel update

--- Infrastructure/DataModel/HolidayPeriodDataModel.cs
+++ Infrastructure/DataModel/HolidayPeriodDataModel.cs
@@ -14,6 +14,6 @@ public class HolidayPeriodDataModel
     public HolidayPeriodDataModel(HolidayPeriod holidayPeriod)
     {
         Id = holidayPeriod.GetId();
-        PeriodDate = new PeriodDateDataModel(holidayPeriod.GetPeriodDate());
+        PeriodDate = new PeriodDateDataModel((PeriodDate)holidayPeriod.GetPeriodDate());
     }
 }
