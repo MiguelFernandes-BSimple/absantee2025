745d9429188cff5e2a2d1376a96a98f8647980c2
MiguelFernandes-BSimple <miguel.fernandes@b-simple.pt>
2025-04-09 13:43:31 +0100

    Collaborator factories

---
+++ Domain/Factory/CheckCollaboratorFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.IRepository;
+using Domain.Models;
+
+namespace Domain.Factory
+{
+    public class CheckCollaboratorFactory : ICheckCollaboratorFactory
+    {
+        private readonly ICollaboratorRepository _repository;
+
+        public CheckCollaboratorFactory(ICollaboratorRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICollaborator Create(IUser user, IPeriodDateTime periodDateTime)
+        {
+            ICollaborator collab = new Collaborator(user, periodDateTime);
+
+            if (_repository.isRepeated(collab))
+                throw new ArgumentException("Collaborator already exists");
+
+            return collab;
+        }
+    }
+}
