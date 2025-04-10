77bb453db45f5acf4c588d64412eaea42a5ca10a
José Malheiro <josemalheiro@b-simple.local>
2025-04-10 15:39:26 +0100

    Fix IAssociationPorjectCollaborator

--- Domain/Interfaces/IAssociationProjectColaborator.cs
+++ Domain/Interfaces/IAssociationProjectColaborator.cs
@@ -5,5 +5,6 @@ public interface IAssociationProjectCollaborator
     public long GetId();
     public long GetCollaboratorId();
     public long GetProjectId();
+    public IPeriodDate GetPeriodDate();
     public bool AssociationIntersectPeriod(IPeriodDate periodDate);
 }
