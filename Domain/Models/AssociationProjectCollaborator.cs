a84ed4f3359c23a96b98936a81e5f6acc7938bbf
José Malheiro <josemalheiro@b-simple.local>
2025-04-10 15:27:40 +0100

    Fixed merge error

--- Domain/Models/AssociationProjectCollaborator.cs
+++ Domain/Models/AssociationProjectCollaborator.cs
@@ -57,33 +57,6 @@ public class AssociationProjectCollaborator : IAssociationProjectCollaborator
         return _periodDate;
     }
 
-<<<<<<< HEAD
-=======
-    public ICollaborator GetCollaborator()
-    {
-        return _collaborator;
-    }
-
-    public IProject GetProject()
-    {
-        return _project;
-    }
-
-    public bool HasCollaborator(ICollaborator collaborator)
-    {
-        return this._collaborator.Equals(collaborator);
-    }
-
-    public bool HasProject(IProject project)
-    {
-        return this._project.Equals(project);
-    }
-    public bool HasProjectIds(long projectIds)
-    {
-        return this._project.GetId() == projectIds;
-    }
-
->>>>>>> 6eae424780baeafde92df33231ec6a1061358a97
     public bool AssociationIntersectPeriod(IPeriodDate periodDate)
     {
         return _periodDate.Intersects(periodDate);
