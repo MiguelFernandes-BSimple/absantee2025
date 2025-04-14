ba6cd6ea178c80ae73a67b5f60c00678b94dfdfa
Fernando Silva <fernando.silva@b-simple.com>
2025-04-14 09:46:38 +0100

    hr manager factory test fix

--- Domain/Factory/HRManager/HRManagerFactory.cs
+++ Domain/Factory/HRManager/HRManagerFactory.cs
@@ -20,7 +20,7 @@ public class HRManagerFactory : IHRManagerFactory
         IUser? user = _userRepository.GetById(userId);
 
         if (user == null)
-            throw new ArgumentException("User dont exists");
+            throw new ArgumentException("User does not exist");
 
         if (user.DeactivationDateIsBefore(periodDateTime.GetFinalDate()))
             throw new ArgumentException("Invalid Arguments");
@@ -28,26 +28,26 @@ public class HRManagerFactory : IHRManagerFactory
         if (user.IsDeactivated())
             throw new ArgumentException("Invalid Arguments");
 
-        HRManager hRManager = new HRManager(userId,periodDateTime);
+        HRManager hRManager = new HRManager(userId, periodDateTime);
 
         return hRManager;
     }
 
     public HRManager Create(long userId, DateTime initDate)
-        {
-            var user = _userRepository.GetById(userId);
+    {
+        var user = _userRepository.GetById(userId);
 
-            if (user == null)
-                throw new ArgumentException("User does not exist");
+        if (user == null)
+            throw new ArgumentException("User does not exist");
 
-            else if (user.DeactivationDateIsBefore(initDate))
-                throw new ArgumentException("Deactivation date is before init date");
+        else if (user.DeactivationDateIsBefore(initDate))
+            throw new ArgumentException("Deactivation date is before init date");
 
-            else if (user.IsDeactivated())
-                throw new ArgumentException("User is deactivated");
+        else if (user.IsDeactivated())
+            throw new ArgumentException("User is deactivated");
 
-            else return new HRManager(userId, initDate);
-        }
+        else return new HRManager(userId, initDate);
+    }
 
     public HRManager Create(IHRManagerVisitor visitor)
     {
