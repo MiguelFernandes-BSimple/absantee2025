0f6c6c5a5e76c402e8c2b5335a214755440b4800
Bruno <bruno.lopes@b-simple.pt>
2025-04-16 12:59:28 +0100

    project tests finished

---
+++ Domain.Tests/ProjectTests/Constructor.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Models;
+using Moq;
+
+namespace Domain.Tests.ProjectTests
+{
+    public class Constructor
+    {
+        [Fact]
+        public void WhenPassingValidData_ThenCreatesProject()
+        {
+            new Project(1, "Title", "TTL", It.IsAny<IPeriodDate>());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Este título tem facilmente mais de cinquenta caracteres.")]
+        public void WhenPassingInvalidTitle_ThenThrowsException(string title)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new Project(1, title, "PJT", It.IsAny<IPeriodDate>())
+            );
+
+            Assert.Equal("Invalid Arguments", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("pjt")]
+        [InlineData("PJT@")]
+        [InlineData("pjtJtjtjtjt")]
+        public void WhenPassingInvalidAcronym_ThenThrowsException(string acronym)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                //act
+                new Project(1, "Title", acronym, It.IsAny<IPeriodDate>())
+            );
+
+            Assert.Equal("Invalid Arguments", exception.Message);
+        }
+    }
+}
