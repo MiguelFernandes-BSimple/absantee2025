cd872f8829b55cc86225a4346edebf4f21c28fe2
Bruno <bruno.lopes@b-simple.pt>
2025-04-17 14:52:07 +0100

    associtaion duplication removed

--- Domain/Interfaces/IAssociationProjectColaborator.cs
+++ Domain/Interfaces/IAssociationProjectColaborator.cs
@@ -8,6 +8,5 @@ public interface IAssociationProjectCollaborator
     public long GetId();
     public long GetCollaboratorId();
     public long GetProjectId();
-    public PeriodDate _periodDate { get; set; }
     public bool AssociationIntersectPeriod(PeriodDate periodDate);
 }
