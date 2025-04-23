5fd769e0ef1ab7908baf91420c371fb2258e19b0
José Malheiro <josemalheiro@b-simple.local>
2025-04-23 10:41:52 +0100

    Training subject repo test done

--- Infrastructure.Tests/TrainingSubjectRepositoryTests/TrainingSubjectRepositoryGetByIdAsyncTests.cs
+++ Infrastructure.Tests/TrainingSubjectRepositoryTests/TrainingSubjectRepositoryGetByIdAsyncTests.cs
@@ -1,10 +1,11 @@
 using AutoMapper;
 using Domain.Interfaces;
-using Infrastructure;
 using Infrastructure.DataModel;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+
+namespace Infrastructure.Tests.TrainingSubjectRepositoryTests;
 
 public class TrainingSubjectRepositoryGetByIdAsyncTests
 {
@@ -19,12 +20,14 @@ public class TrainingSubjectRepositoryGetByIdAsyncTests
         using var context = new AbsanteeContext(options);
 
         var trainingSubject1 = new Mock<ITrainingSubject>();
+        trainingSubject1.Setup(t => t.Id).Returns(1);
         trainingSubject1.Setup(t => t.Subject).Returns("Subject1");
         trainingSubject1.Setup(t => t.Description).Returns("Description1");
         var trainingSubject1DM = new TrainingSubjectDataModel(trainingSubject1.Object);
         context.TrainingSubjects.Add(trainingSubject1DM);
 
         var trainingSubject2 = new Mock<ITrainingSubject>();
+        trainingSubject2.Setup(t => t.Id).Returns(2);
         trainingSubject2.Setup(t => t.Subject).Returns("Subject2");
         trainingSubject2.Setup(t => t.Description).Returns("Description2");
         var trainingSubject2DM = new TrainingSubjectDataModel(trainingSubject2.Object);
@@ -32,17 +35,56 @@ public class TrainingSubjectRepositoryGetByIdAsyncTests
 
         await context.SaveChangesAsync();
 
-        var filteredDMs = new List<TrainingSubjectDataModel>() { trainingSubject2DM };
-        var expected = new List<ITrainingSubject>() { trainingSubject2.Object };
+        var tsToSearchId = 2;
+        ITrainingSubject expected = trainingSubject2.Object;
 
         var mapper = new Mock<IMapper>();
 
-        var trainingModuleRepo = new TrainingSubjectRepository(context, mapper.Object);
+        var trainingSubjectRepository = new TrainingSubjectRepository(context, mapper.Object);
 
         //Act
-        var result = await trainingModuleRepo.IsDuplicated("Subject2");
+        var result = await trainingSubjectRepository.GetByIdAsync(tsToSearchId);
 
         //Assert
-        Assert.True(result);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public async Task WhenPassingInvalidId_ThenReturnNull()
+    {
+        //Assert
+        var options = new DbContextOptionsBuilder<AbsanteeContext>()
+           .UseInMemoryDatabase(Guid.NewGuid().ToString()) // ensure isolation per test
+           .Options;
+
+        using var context = new AbsanteeContext(options);
+
+        var trainingSubject1 = new Mock<ITrainingSubject>();
+        trainingSubject1.Setup(t => t.Id).Returns(1);
+        trainingSubject1.Setup(t => t.Subject).Returns("Subject1");
+        trainingSubject1.Setup(t => t.Description).Returns("Description1");
+        var trainingSubject1DM = new TrainingSubjectDataModel(trainingSubject1.Object);
+        context.TrainingSubjects.Add(trainingSubject1DM);
+
+        var trainingSubject2 = new Mock<ITrainingSubject>();
+        trainingSubject2.Setup(t => t.Id).Returns(2);
+        trainingSubject2.Setup(t => t.Subject).Returns("Subject2");
+        trainingSubject2.Setup(t => t.Description).Returns("Description2");
+        var trainingSubject2DM = new TrainingSubjectDataModel(trainingSubject2.Object);
+        context.TrainingSubjects.Add(trainingSubject2DM);
+
+        await context.SaveChangesAsync();
+
+        var tsToSearchId = 3;
+
+        var mapper = new Mock<IMapper>();
+
+        var trainingSubjectRepository = new TrainingSubjectRepository(context, mapper.Object);
+
+        //Act
+        var result = await trainingSubjectRepository.GetByIdAsync(tsToSearchId);
+
+        //Assert
+        Assert.Null(result);
     }
 }
