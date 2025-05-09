516fd9b03796d328ddd4a55d63de6eaafe528015
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-05-09 17:00:43 +0100

    fix GetById CollaboratorRepoTests

--- Infrastructure.Tests/CollaboratorRepositoryTests/CollaboratorRepositoryGetByIdAsyncTests.cs
+++ Infrastructure.Tests/CollaboratorRepositoryTests/CollaboratorRepositoryGetByIdAsyncTests.cs
@@ -1,99 +1,82 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Domain.Interfaces;
-//using Domain.Models;
-//using Domain.Visitor;
-//using Infrastructure.DataModel;
-//using AutoMapper;
-//using Infrastructure.Repositories;
-//using Microsoft.EntityFrameworkCore;
-//using Moq;
+﻿using Domain.Interfaces;
+using Domain.Visitor;
+using Infrastructure.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Infrastructure.Repositories;
+using Domain.Models;
 
-//namespace Infrastructure.Tests.CollaboratorRepositoryTests
-//{
-//    public class CollaboratorRepositoryGetByIdAsyncTests
-//    {
-//        [Fact]
-//        public async Task WhenSearchingById_ThenReturnsCollaboratorWithId()
-//        {
-//            // Arrange
-//            var options = new DbContextOptionsBuilder<AbsanteeContext>()
-//                .UseInMemoryDatabase(Guid.NewGuid().ToString()) // ensure isolation per test
-//                .Options;
+namespace Infrastructure.Tests.CollaboratorRepositoryTests
+{
+    public class CollaboratorRepositoryGetByIdAsyncTests : RepositoryTestBase
+    {
+        [Fact]
+        public async Task WhenSearchingById_ThenReturnsCollaborator()
+        {
+            // Arrange
+            var collaborator1 = new Mock<ICollaborator>();
+            var guid1 = Guid.NewGuid();
+            var userguid1 = Guid.NewGuid();
+            var period1 = new PeriodDateTime(DateTime.Today.AddDays(-1), DateTime.Today);
+            collaborator1.Setup(c => c.Id).Returns(guid1);
+            collaborator1.Setup(c => c.UserId).Returns(userguid1);
+            var collaboratorDM1 = new CollaboratorDataModel(collaborator1.Object);
+            context.Collaborators.Add(collaboratorDM1);
 
-//            using var context = new AbsanteeContext(options);
+            var collaborator2 = new Mock<ICollaborator>();
+            var guid2 = Guid.NewGuid();
+            collaborator1.Setup(c => c.Id).Returns(guid2);
+            var collaboratorDM2 = new CollaboratorDataModel(collaborator2.Object);
+            context.Collaborators.Add(collaboratorDM2);
 
-//            var collaborator1 = new Mock<ICollaborator>();
-//            collaborator1.Setup(c => c.Id).Returns(1);
-//            var collaboratorDM1 = new CollaboratorDataModel(collaborator1.Object);
-//            context.Collaborators.Add(collaboratorDM1);
+            await context.SaveChangesAsync();
 
-//            var collaborator2 = new Mock<ICollaborator>();
-//            collaborator2.Setup(c => c.Id).Returns(2);
-//            var collaboratorDM2 = new CollaboratorDataModel(collaborator2.Object);
-//            context.Collaborators.Add(collaboratorDM2);
+            var expected = new Mock<ICollaborator>().Object;
 
-//            var collaborator3 = new Mock<ICollaborator>();
-//            collaborator3.Setup(c => c.Id).Returns(3);
-//            var collaboratorDM3 = new CollaboratorDataModel(collaborator3.Object);
-//            context.Collaborators.Add(collaboratorDM3);
+            _mapper.Setup(m => m.Map<CollaboratorDataModel, Collaborator>(
+            It.Is<CollaboratorDataModel>(t =>
+                t.Id == collaboratorDM1.Id
+                )))
+                .Returns(new Collaborator(collaboratorDM1.Id, collaboratorDM1.UserId, collaboratorDM1.PeriodDateTime));
 
-//            await context.SaveChangesAsync();
+            var collaboratorRepository = new CollaboratorRepositoryEF(context, _mapper.Object);
+            //Act 
+            var result = await collaboratorRepository.GetByIdAsync(guid1);
 
-//            var expected = new Mock<ICollaborator>().Object;
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(collaboratorDM1.Id, result.Id);
+        }
 
-//            var collabMapper = new Mock<IMapper<ICollaborator, ICollaboratorVisitor>>();
-//            collabMapper.Setup(cm => cm.ToDomain(collaboratorDM3)).Returns(expected);
+        [Fact]
+        public async Task WhenSearchingByIdWithNoCollaborators_ThenReturnsNull()
+        {
+            // Arrange
+            var collaborator1 = new Mock<ICollaborator>();
+            var guid1 = Guid.NewGuid();
+            var guid2 = Guid.NewGuid();
+            var userguid1 = Guid.NewGuid();
+            var period1 = new PeriodDateTime(DateTime.Today.AddDays(-1), DateTime.Today);
+            collaborator1.Setup(c => c.Id).Returns(guid1);
+            collaborator1.Setup(c => c.UserId).Returns(userguid1);
+            var collaboratorDM1 = new CollaboratorDataModel(collaborator1.Object);
+            context.Collaborators.Add(collaboratorDM1);
 
-//            var collaboratorRepository = new CollaboratorRepository(context, collabMapper.Object);
-//            //Act 
-//            var result = await collaboratorRepository.GetByIdAsync(3);
+            await context.SaveChangesAsync();
 
-//            //Assert
-//            Assert.Equal(expected, result);
-//        }
+            _mapper.Setup(m => m.Map<CollaboratorDataModel, Collaborator>(
+           It.Is<CollaboratorDataModel>(t =>
+               t.Id == collaboratorDM1.Id
+               )))
+               .Returns(new Collaborator(collaboratorDM1.Id, collaboratorDM1.UserId, collaboratorDM1.PeriodDateTime));
 
-//        [Fact]
-//        public async Task WhenSearchingByIdWithNoCollaborators_ThenReturnsNull()
-//        {
-//            // Arrange
-//            var options = new DbContextOptionsBuilder<AbsanteeContext>()
-//                .UseInMemoryDatabase(Guid.NewGuid().ToString()) // ensure isolation per test
-//                .Options;
+            var collaboratorRepository = new CollaboratorRepositoryEF(context, _mapper.Object);
 
-//            using var context = new AbsanteeContext(options);
+            //Act 
+            var result = await collaboratorRepository.GetByIdAsync(guid2);
 
-//            var collaborator1 = new Mock<ICollaborator>();
-//            collaborator1.Setup(c => c.Id).Returns(1);
-//            var collaboratorDM1 = new CollaboratorDataModel(collaborator1.Object);
-//            context.Collaborators.Add(collaboratorDM1);
-
-//            var collaborator2 = new Mock<ICollaborator>();
-//            collaborator2.Setup(c => c.Id).Returns(2);
-//            var collaboratorDM2 = new CollaboratorDataModel(collaborator2.Object);
-//            context.Collaborators.Add(collaboratorDM2);
-
-//            var collaborator3 = new Mock<ICollaborator>();
-//            collaborator3.Setup(c => c.Id).Returns(3);
-//            var collaboratorDM3 = new CollaboratorDataModel(collaborator3.Object);
-//            context.Collaborators.Add(collaboratorDM3);
-
-//            await context.SaveChangesAsync();
-
-//            var expected = new Mock<ICollaborator>().Object;
-
-//            var collabMapper = new Mock<IMapper<ICollaborator, ICollaboratorVisitor>>();
-//            collabMapper.Setup(cm => cm.ToDomain(collaboratorDM3)).Returns(expected);
-
-//            var collaboratorRepository = new CollaboratorRepository(context, collabMapper.Object);
-//            //Act 
-//            var result = await collaboratorRepository.GetByIdAsync(4);
-
-//            //Assert
-//            Assert.Null(result);
-//        }
-//    }
-//}
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
