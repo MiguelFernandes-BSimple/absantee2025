b0e6588bdc6b99634af7156d8c63ab974f0b5be5
gbarbosa <goncalo.barbosa@b-simple.pt>
2025-06-20 12:45:49 +0100

    ChangeTestsAndCreateNewOnes

--- WebApi.IntegrationTests/Tests/TrainingModuleControllerTests.cs
+++ WebApi.IntegrationTests/Tests/TrainingModuleControllerTests.cs
@@ -1,4 +1,4 @@
-/* using Application.DTO.TrainingModule;
+using Application.DTO.TrainingModule;
 using Application.DTO.TrainingSubject;
 using WebApi.IntegrationTests.Helpers;
 using Xunit;
@@ -37,76 +37,4 @@ public class TrainingModuleControllerTests : IntegrationTestBase, IClassFixture<
         Assert.Equal(trainingModuleDTO.TrainingSubjectId, createdTrainingModuleDTO.TrainingSubjectId);
         Assert.True(trainingModuleDTO.Periods.SequenceEqual(createdTrainingModuleDTO.Periods));
     }
-
-    [Fact]
-    public async Task AddCollaboratorToModule_Return201Created()
-    {
-        // Arrange
-        // Create a random training subject payload
-        var trainingSubjectDTO =
-                    TrainingSubjectHelper.GenerateRandomAddTrainingSubjectDTO();
-
-        // Post new subject and get Id
-        var createdTrainingSubjectDTO =
-                    await PostAndDeserializeAsync<TrainingSubjectDTO>("/api/trainingsubjects", trainingSubjectDTO);
-
-        // Create a training module payload with random dates
-        var trainingModuleDTO =
-            TrainingModuleHelper.GenerateAddTrainingModuleDTORandomDates(createdTrainingSubjectDTO.Id);
-
-        // Post new module
-        var createdTrainingModuleDTO =
-            await PostAndDeserializeAsync<TrainingModuleDTO>("/api/trainingmodules", trainingModuleDTO);
-
-
-        // Create a new Collaborator to add to module
-        var collaboratorDTO = CollaboratorHelper.GenerateRandomCollaboratorDto();
-
-        // Post new collaborator to add 
-        var createdCollaboratorDTO =
-            await PostAndDeserializeAsync<CollaboratorDTO>("/api/collaborators", collaboratorDTO);
-
-        var assocTMCDTO =
-            AssociationTrainingModuleCollaboratorHelper.GenerateCreateAssociationTrainingModuleCollaboratorDTO(createdCollaboratorDTO.Id);
-
-        // Act : Add new collaborator to module
-        var createdAssocDTO =
-            await PostAndDeserializeAsync<AssociationTrainingModuleCollaboratorDTO>($"/api/trainingmodules/{createdTrainingModuleDTO.Id}/collaborators", assocTMCDTO);
-
-        // Assert
-        Assert.NotNull(createdAssocDTO);
-        Assert.Equal(createdTrainingModuleDTO.Id, createdAssocDTO.TrainingModuleId);
-        Assert.Equal(createdCollaboratorDTO.Id, createdAssocDTO.CollaboratorId);
-    }
-
-    [Fact]
-    public async Task GetActiveCollaboratorsWithNoTrainingDoneForSubject_Return200Ok()
-    {
-        // Arrange
-        var iniCollabdIds =
-            await GetAndDeserializeAsync<IEnumerable<Guid>>($"/api/collaborators");
-        
-        // Create a random training subject payload
-        var trainingSubjectDTO =
-                    TrainingSubjectHelper.GenerateRandomAddTrainingSubjectDTO();
-
-        // Post new subject and get Id
-        var createdTrainingSubjectDTO =
-                    await PostAndDeserializeAsync<TrainingSubjectDTO>("/api/trainingsubjects", trainingSubjectDTO);
-
-        // Create a training module payload with random dates
-        var trainingModuleDTO =
-            TrainingModuleHelper.GenerateAddTrainingModuleDTORandomDates(createdTrainingSubjectDTO.Id);
-
-        // Post new module
-        var createdTrainingModuleDTO =
-            await PostAndDeserializeAsync<TrainingModuleDTO>("/api/trainingmodules", trainingModuleDTO);
-
-        // Act
-        var collabdIds =
-            await GetAndDeserializeAsync<IEnumerable<Guid>>($"/api/trainingmodules/not-completed/subjects/{createdTrainingSubjectDTO.Id}/collaborators/active/");
-
-        // Assert
-        Assert.True(iniCollabdIds.SequenceEqual(collabdIds));
-    }
-} */
+}
